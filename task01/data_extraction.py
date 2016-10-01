1964931cd6692031b542ee1f6883b7d1faeaeb8f
Bugaevsky Vladimir <bugaevsky@mail.ru>
2016-10-01 19:45:58 +0300

    optimized readings of dataset

--- task01/data_extraction.py
+++ task01/data_extraction.py
@@ -10,14 +10,14 @@ IMAGES_TRAIN = 'train-images.idx3-ubyte'
 LABELS_TRAIN = 'train-labels.idx1-ubyte'
 
 
-def get_hex_from_file(f, b):
-    return int(f.read(b).encode('hex'), 16)
+def string_to_int(str):
+    return int(str.encode('hex'), 16)
 
 
 def read_label_file(file_name, n_objects=None, offset=0):
     with open(PATH + file_name, 'rb') as f:
-        magic = get_hex_from_file(f, 4)
-        n_items = get_hex_from_file(f, 4)
+        buffer = f.read(8)
+        magic, n_items = map(string_to_int, [buffer[i:i + 4] for i in range(0, len(buffer), 4)])
 
         n_size = n_objects if n_objects else n_items - offset
         if offset >= n_items:
@@ -26,14 +26,15 @@ def read_label_file(file_name, n_objects=None, offset=0):
             n_size = n_items - offset
 
         f.seek(offset, 1)
-        labels = [get_hex_from_file(f, 1) for i in range(n_size)]
+        buffer = f.read(n_size)
+        labels = [string_to_int(buffer[i]) for i in range(n_size)]
     return labels
 
 
 def read_image_file(file_name, n_objects=None, offset=0):
     with open(PATH + file_name, 'rb') as f:
-        magic = get_hex_from_file(f, 4)
-        n_items = get_hex_from_file(f, 4)
+        buffer = f.read(16)
+        magic, n_items, n_rows, n_cols = map(string_to_int, [buffer[i:i + 4] for i in range(0, len(buffer), 4)])
 
         n_size = n_objects if n_objects else n_items - offset
         if offset >= n_items:
@@ -41,12 +42,12 @@ def read_image_file(file_name, n_objects=None, offset=0):
         elif offset + n_size > n_items:
             n_size = n_items - offset
 
-        n_rows = get_hex_from_file(f, 4)
-        n_cols = get_hex_from_file(f, 4)
-
         f.seek(offset * n_cols * n_rows, 1)
-        images = [np.array([[get_hex_from_file(f, 1) for col in range(n_cols)] for row in range(n_rows)])
-                  for i in range(n_size)]
+
+        buffer = f.read(n_size * n_cols * n_rows)
+        images = [np.asarray(
+            [[string_to_int(buffer[i + n_cols * row + col]) for col in range(n_cols)] for row in range(n_rows)]
+        ) for i in range(0, n_size * n_cols * n_rows, n_cols * n_rows)]
 
     return images
 
@@ -56,6 +57,7 @@ if __name__ == '__main__':
     labels = read_label_file(LABELS_TRAIN, batch_size, offset)
     images = read_image_file(IMAGES_TRAIN, batch_size, offset)
 
+    print labels
     n, m = 10, 10
     f, axarr = plt.subplots(n, m, figsize=(6, 6))
     for i in range(n):
